b38e0ad4a8d224d9a73747d03bb010ed5aae7dd4
harsh456priyanshu <harshpriyanshu456@gmail.com>
2025-06-29 08:47:51 +0530

    Fix: Use HashRouter for GitHub Pages deployment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import react from 'react';
-import ReactDOM from 'react-dom'
+// import react from 'react';
+// import ReactDOM from 'react-dom'
 
+// import App from './App';
+// import './index.css';
+// import {ContextProvider} from './contexts/ContextProvider'
+
+// ReactDOM.render(
+// <ContextProvider>
+//     <App/>
+// </ContextProvider>
+// , document.getElementById('root')
+// ); 
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import {ContextProvider} from './contexts/ContextProvider'
+import { ContextProvider } from './contexts/ContextProvider';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-<ContextProvider>
-    <App/>
-</ContextProvider>
-, document.getElementById('root')
-); 
+  <HashRouter>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </HashRouter>,
+  document.getElementById('root')
+);
